c88438717c4b21f5c7e6beb4a880c2dabc1b5ffb
Ken Yeh <yehken@gmail.com>
2023-07-25 11:56:20 -0700

    Feature 16 Complete

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import TaskForm
+from .models import Task
 # Create your views here.
 
 
@@ -18,3 +19,13 @@ def create_task(request):
         "form": form,
     }
     return render(request, "tasks/create.html", context)
+
+
+# this view will only show tasks assigned to logged in user
+@login_required
+def show_my_tasks(request):
+    tasks = Task.objects.filter(assignee=request.user)
+    context = {
+        "tasks": tasks,
+    }
+    return render(request, "tasks/mine.html", context)
