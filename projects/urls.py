e350b376f5c31aa07ea98a0639bd95dcd0d4e30e
Ken Yeh <yehken@gmail.com>
2023-07-25 11:39:07 -0700

    Feature 15 Complete

--- projects/urls.py
+++ projects/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from projects.views import list_projects, show_project, create_project
+from .views import list_projects, show_project, create_project
 
 
 urlpatterns = [
